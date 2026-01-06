5d7649c1a62d292db5872b12c44bbbccd1684a38
Boyan Kloosterman <kloostermanboyan@gmail.com>
2026-01-06 13:36:16 +0100

    Update eslint.config.mjs

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -5,53 +5,55 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+    {
+        ignores: ['eslint.config.mjs'],
     },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-      '@typescript-eslint/naming-convention': [
-        'warn',
-        {
-          selector: 'variable',
-          format: ['camelCase', 'UPPER_CASE'],
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    eslintPluginPrettierRecommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.jest,
+            },
+            sourceType: 'commonjs',
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
         },
-        {
-          selector: 'function',
-          format: ['camelCase'],
-        },
-        {
-          selector: 'typeLike',
-          format: ['PascalCase'],
-        },
-      ],
-      'prettier/prettier': [
-        'error',
-        {
-          endOfLine: 'auto',
-          tabWidth: 4,
-          useTabs: false,
-        },
-      ],
     },
-  },
+    {
+        rules: {
+            '@typescript-eslint/no-unused-vars': 'off',
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/no-floating-promises': 'warn',
+            '@typescript-eslint/no-unsafe-argument': 'warn',
+            '@typescript-eslint/naming-convention': [
+                'warn',
+                {
+                    selector: 'variable',
+                    format: ['camelCase', 'UPPER_CASE'],
+                },
+                {
+                    selector: 'function',
+                    format: ['camelCase'],
+                },
+                {
+                    selector: 'typeLike',
+                    format: ['PascalCase'],
+                },
+            ],
+            'prettier/prettier': [
+                'error',
+                {
+                    endOfLine: 'auto',
+                    tabWidth: 4,
+                    useTabs: false,
+                },
+            ],
+        },
+    },
 );
