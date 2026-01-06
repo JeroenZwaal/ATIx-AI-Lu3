62f1d9230897fc551ce46aef0cb7d18d059647dc
Boyan Kloosterman <kloostermanboyan@gmail.com>
2026-01-06 13:56:37 +0100

    Refactor ESLint config and add Prettier integration
    Refactored the ESLint configuration for improved readability and maintainability. Added and configured eslint-plugin-prettier and eslint-config-prettier to integrate Prettier formatting with ESLint.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -7,37 +7,32 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  reactHooks.configs.flat.recommended,
-  reactRefresh.configs.vite,
-  eslintPluginPrettierRecommended,
-  {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    globalIgnores(['dist']),
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    reactHooks.configs.flat.recommended,
+    reactRefresh.configs.vite,
+    eslintPluginPrettierRecommended,
+    {
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/naming-convention': [
+                'warn',
+                {
+                    selector: 'variable',
+                    format: ['camelCase', 'UPPER_CASE'],
+                },
+                {
+                    selector: 'typeLike',
+                    format: ['PascalCase'],
+                },
+            ],
+            'prettier/prettier': ['error', { endOfLine: 'auto' }],
+        },
     },
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/naming-convention': [
-        'warn',
-        {
-          selector: 'variable',
-          format: ['camelCase', 'UPPER_CASE'],
-        },
-        {
-          selector: 'function',
-          format: ['camelCase'],
-        },
-        {
-          selector: 'typeLike',
-          format: ['PascalCase'],
-        },
-      ],
-      'prettier/prettier': ['error', { endOfLine: 'auto' }],
-    },
-  },
 ]);
-
